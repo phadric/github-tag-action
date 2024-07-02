91d077006f78090f4484b086b97f5c1246de4a58
phadric <phadric@gmail.com>
2024-07-02 12:50:12 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -164,13 +164,7 @@ case "$log" in
     *$major_string_token* ) new=$(semver -i major "$tag"); part="major";;
     *$minor_string_token* ) new=$(semver -i minor "$tag"); part="minor";;
     *$patch_string_token* ) new=$(semver -i patch "$tag"); part="patch";;
-    *$none_string_token* )
-        echo "Default bump was set to none. Skipping..."
-        setOutput "old_tag" "$tag"
-        setOutput "new_tag" "$tag"
-        setOutput "tag" "$tag"
-        setOutput "part" "$default_semvar_bump"
-        exit 0;;
+    *$none_string_token* ) new=$tag; part="none";;
     * )
         if [ "$default_semvar_bump" == "none" ]
         then
